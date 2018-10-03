d376bfb3e2e16e10583c2f5b25fd6a3489b297a5
Stefano Lupo <lupos@tcd.ie>
2018-10-03 21:58:34 +0100

    Added command scripts

--- practical4/commands.sh
+++ practical4/commands.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# command=""
+potfile_path="practical4.potfile"
+potfile_flag="--potfile-path="
+
 
 if [ "$1" == "DES" ] 
 then
 	command="hashcat -m 1500 -a 0 DES.hashes ../dictionaries/rockyou.txt"
 elif [ "$1" == "MD5Crypt" ]
 then
-	command="hashcat -a 0 -m 500 -O md5crypt.hashes ../dictionaries/rockyou.txt2"
+	command="hashcat -a 0 -m 500 -O md5crypt.hashes ../dictionaries/rockyou.txt"
+elif [ "$1" == "SHA256" ]
+then
+	command="hashcat -a 0 -m 7400 -O sha256.hashes ../dictionaries/rockyou.txt"
 else
-	command="nothing"
+	command="Unknown command: " $1
 fi
 
-echo $command
+echo $command $potfile_flag $potfile_path

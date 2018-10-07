e1a0871c995e06a15bc05b49108490b3d9275a77
Stefano Lupo <lupos@tcd.ie>
2018-10-08 00:48:55 +0100

    Clean up old sessions

--- practical4/scripts/format.sh
+++ practical4/scripts/format.sh
@@ -31,26 +31,26 @@ printf "%d solved hashes before sorting\n" $(wc -l < $tmpfile)
 sort -u -o $tmpfile $tmpfile
 printf "%d solved hashes after sorting\n" $(wc -l < $tmpfile)
 
-echo "" > $logfile
+# echo "" > $logfile
 
-printf "Mismatched Hashes:" >> $logfile
-cat $mismatched_hashes >> $logfile
-printf "\n" >> $logfile
+# printf "Mismatched Hashes:" >> $logfile
+# cat $mismatched_hashes >> $logfile
+# printf "\n" >> $logfile
 
 # Fix mismatched DES passwords
 next_line=""
 while read line; do
-  echo "Checking line: " $line >> $logfile
+  # echo "Checking line: " $line >> $logfile
   next_line=$line
   while read mismatched; do
-    echo "Checking against: " $mismatched >> $logfile
+    # echo "Checking against: " $mismatched >> $logfile
     if [ "$line" == "$mismatched" ]; then
       next_line=${line:: -1}
       break
     fi
   done < $mismatched_hashes
   echo $next_line  >> $tmpfile2
-  printf "\n" >> $logfile
+  # printf "\n" >> $logfile
 done < $tmpfile
 
 cat $tmpfile2 > $outfile

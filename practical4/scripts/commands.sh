d1a970c319e6f9c310d73761c677d876d880078e
Stefano Lupo <lupos@tcd.ie>
2018-10-07 01:20:34 +0100

    MD5 Rockyou jumbos (#6)

--- practical4/scripts/commands.sh
+++ practical4/scripts/commands.sh
@@ -4,27 +4,27 @@ potfile_path="practical4.potfile"
 potfile_flag="--potfile-path="
 
 
-if [ "$1" == "DES" ] then
+if [ "$1" == "DES" ]; then
 	command="hashcat -m 1500 -a 0 hashes/DES.hashes ../dictionaries/rockyou.txt"
 
-elif [ "$1" == "MD5Crypt" ] then
+elif [ "$1" == "MD5Crypt" ]; then
 	command="hashcat -a 0 -m 500 -O hashes/MD5Crypt.hashes ../dictionaries/rockyou.txt"
 
-elif [ "$1" == "SHA256" ] then
+elif [ "$1" == "SHA256" ]; then
 	command="hashcat -a 0 -m 7400 -O hashes/SHA256.hashes ../dictionaries/rockyou.txt"
 
-elif [ "$1" == "SHA512" ] then
+elif [ "$1" == "SHA512" ]; then
 	command="hashcat -a 0 -m 1800 -O hashes/SHA512.hashes ../dictionaries/rockyou.txt"
 
 # Could be DJANGO -m -10000 or CISCO-IOS -m 9200
-# elif [ "$1" == "pbkdf2" ] then
-# 	command="hashcat -a 0 -m 10000 -O hashes/pbkdf2.hashes ../dictionaries/rockyou.txt"
+elif [ "$1" == "pbkdf2" ]; then
+	command="hashcat -a 0 -m 10000 -O hashes/pbkdf2.hashes ../dictionaries/rockyou.txt"
 
-# elif [ "$1" == "argon" ] then
-# 	command="hashcat -a 0 -m 7400 -O hashes/argon.hashes ../dictionaries/rockyou.txt"
+elif [ "$1" == "argon" ]; then
+	command="hashcat -a 0 -m 7400 -O hashes/argon.hashes ../dictionaries/rockyou.txt"
 
 else
 	command="Unknown command: " $1
 fi
 
-echo $command $potfile_flag $potfile_path
+echo $command $potfile_flag$potfile_path
